00ce4a0024eee9e8de59267f91fb125daba44e43
sophiamziou <sofiamziou@gmail.com>
2023-02-16 09:03:04 +0100

    exercise done

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $title = 'Hello world!';
+    return view('home', compact('title'));
 });
